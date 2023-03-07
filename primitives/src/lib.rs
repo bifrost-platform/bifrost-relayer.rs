518d632f33e8897a87dac7fc45ebd2aaf92f21b6
Jinwook Jang <noah@Jinwooks-Mac-mini.local>
2023-03-07 17:05:26 +0900

    CCCP-9 feature: Add basic transaction manager

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -1,16 +1,8 @@
+mod socket_event;
 pub mod traits;
+mod tx_manager;
 
-pub fn add(left: usize, right: usize) -> usize {
-	left + right
-}
+pub use socket_event::*;
+pub use tx_manager::*;
 
-#[cfg(test)]
-mod tests {
-	use super::*;
-
-	#[test]
-	fn it_works() {
-		let result = add(2, 2);
-		assert_eq!(result, 4);
-	}
-}
+pub type Err = Box<dyn std::error::Error + Send + Sync>;
