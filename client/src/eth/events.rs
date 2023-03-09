3342e65f1409dfd620c09f211ad5f6b452e972c8
dnjscksdn98 <dnjscksdn98@gmail.com>
2023-03-09 17:15:43 +0900

    CCCP-6, feature: initialize event detector

---
+++ client/src/eth/events.rs
@@ -0,0 +1,22 @@
+use web3::{types::U64, Transport};
+
+use super::EthClient;
+
+#[derive(Debug, Clone)]
+pub struct EventDetector<T: Transport> {
+	pub client: EthClient<T>,
+	pub queue: Vec<U64>,
+}
+
+impl<T: Transport> EventDetector<T> {
+	pub fn new(client: EthClient<T>) -> Self {
+		Self { client, queue: vec![] }
+	}
+
+	pub async fn run(&self) -> web3::Result<()> {
+		loop {
+			let block_number = self.client.get_block_number().await?;
+			println!("block number: {:?}", block_number);
+		}
+	}
+}
--- primitives/src/traits.rs
+++ primitives/src/traits.rs
@@ -1,11 +1,1 @@
-// use ethers::types::TransactionRequest;
 
-// pub(crate) trait TransactionManager {
-// 	fn build(
-// 		&self,
-// 		_contract_name: &str,
-// 		_method_name: &str,
-// 		socket_event: SocketEvent,
-// 	) -> TransactionRequest;
-// 	fn send(&self);
-// }
