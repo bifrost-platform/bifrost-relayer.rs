2824130356d868b944eac23838add875bca76ac8
noah <jinwook@pi-lab.io>
2023-03-10 18:55:15 +0900

    CCCP-9, refactor: Remove unused custom socket event and update config

--- relayer/src/service.rs
+++ relayer/src/service.rs
@@ -3,7 +3,10 @@ use cccp_primitives::EVMTransactionManager;
 use sc_service::{Error as ServiceError, TaskManager};
 use std::sync::Arc;
 
-use ethers::providers::{Http, Provider};
+use ethers::{
+	providers::{Http, Provider},
+	types::Address,
+};
 
 pub fn relay(config: Configuration) -> Result<TaskManager, ServiceError> {
 	new_relay_base(config).map(|RelayBase { task_manager, .. }| task_manager)
@@ -14,7 +17,9 @@ pub fn new_relay_base(config: Configuration) -> Result<RelayBase, ServiceError> 
 
 	// TODO: Workers tasks should spawn from here.
 	let client = Arc::new(Provider::<Http>::try_from(&config.private_config.eth_provider).unwrap());
-	let mut evm_manager = EVMTransactionManager::new(client);
+	let socket_address = "0x4A31FfeAc276CC5e508cAC0568d932d398C4DD84".parse::<Address>().unwrap();
+
+	let mut evm_manager = EVMTransactionManager::new(client, socket_address).unwrap();
 
 	task_manager.spawn_essential_handle().spawn_blocking(
 		"bfc-tx-manager",
