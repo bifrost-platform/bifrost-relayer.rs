fa7eba6ea7f7f6d1dc72c2b4a31d3030ce2ce299
dnjscksdn98 <dnjscksdn98@gmail.com>
2023-03-10 19:15:14 +0900

    CCCP-6, feature: process confirmed blocks

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -4,3 +4,17 @@ mod tx_manager;
 pub use tx_manager::*;
 
 pub type Err = Box<dyn std::error::Error + Send + Sync>;
+
+/// The time interval used when to request a new block for BIFROST.
+pub const BFC_CALL_INTERVAL_MS: u64 = 2_000;
+
+/// The size of the queue storing blocks to prevent block reorgs for BIFROST.
+pub const BFC_BLOCK_QUEUE_SIZE: u64 = 6;
+
+/// The additional configuration details for an EVM-based chain.
+pub struct EthClientConfiguration {
+	/// The `get_block` request interval in milliseconds.
+	pub call_interval: u64,
+	/// The maximum block queue size for confirmation.
+	pub block_queue_size: u64,
+}
