db7dd030d7c1401252eb39d1083dbfa4715eaeb0
noah <jinwook@pi-lab.io>
2023-03-10 18:55:15 +0900

    CCCP-9, refactor: Remove unused custom socket event and update config

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -1,8 +1,6 @@
-mod socket_event;
 pub mod traits;
 mod tx_manager;
 
-pub use socket_event::*;
 pub use tx_manager::*;
 
 pub type Err = Box<dyn std::error::Error + Send + Sync>;
--- primitives/src/traits.rs
+++ primitives/src/traits.rs
@@ -1,13 +1,11 @@
-use ethers::types::TransactionRequest;
+// use ethers::types::TransactionRequest;
 
-use crate::SocketEvent;
-
-pub(crate) trait TransactionManager {
-	fn build(
-		&self,
-		_contract_name: &str,
-		_method_name: &str,
-		socket_event: SocketEvent,
-	) -> TransactionRequest;
-	fn send(&self);
-}
+// pub(crate) trait TransactionManager {
+// 	fn build(
+// 		&self,
+// 		_contract_name: &str,
+// 		_method_name: &str,
+// 		socket_event: SocketEvent,
+// 	) -> TransactionRequest;
+// 	fn send(&self);
+// }
