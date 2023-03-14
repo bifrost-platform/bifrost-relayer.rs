b7649e2915a0e6fbe827bcad9609b4b444378423
dnjscksdn98 <dnjscksdn98@gmail.com>
2023-03-14 16:02:50 +0900

    CCCP-6, feature: parse socket events

--- primitives/src/eth.rs
+++ primitives/src/eth.rs
@@ -1,8 +1,19 @@
-/// The time interval used when to request a new block for BIFROST.
-pub const BFC_CALL_INTERVAL_MS: u64 = 2_000;
+use web3::types::H160;
 
-/// The size of the queue storing blocks to prevent block reorgs for BIFROST.
-pub const BFC_BLOCK_QUEUE_SIZE: u64 = 6;
+pub mod bfc {
+	/// The time interval used when to request a new block for BIFROST.
+	pub const BFC_CALL_INTERVAL_MS: u64 = 2_000;
+
+	/// The size of the queue storing blocks to prevent block reorgs for BIFROST.
+	pub const BFC_BLOCK_QUEUE_SIZE: u64 = 6;
+
+	/// The socket contract address deployed on BIFROST.
+	pub const BFC_SOCKET_CONTRACT_ADDRESS: &str = "0xd551F33Ca8eCb0Be83d8799D9C68a368BA36Dd52";
+}
+
+/// The socket event signature.
+pub const SOCKET_EVENT_SIG: &str =
+	"0x918454f530580823dd0d8cf59cacb45a6eb7cc62f222d7129efba5821e77f191";
 
 /// The additional configuration details for an EVM-based chain.
 pub struct EthClientConfiguration {
@@ -10,4 +21,6 @@ pub struct EthClientConfiguration {
 	pub call_interval: u64,
 	/// The maximum block queue size for confirmation.
 	pub block_queue_size: u64,
+	/// The socket contract address.
+	pub socket_address: H160,
 }
--- src/service.rs
+++ src/service.rs
@@ -1,9 +1,14 @@
+use std::str::FromStr;
+
 use cc_cli::Configuration;
 use cccp_client::eth::{EthClient, EventDetector};
-use cccp_primitives::eth::{EthClientConfiguration, BFC_BLOCK_QUEUE_SIZE, BFC_CALL_INTERVAL_MS};
+use cccp_primitives::eth::{
+	bfc::{BFC_BLOCK_QUEUE_SIZE, BFC_CALL_INTERVAL_MS, BFC_SOCKET_CONTRACT_ADDRESS},
+	EthClientConfiguration,
+};
 
 use sc_service::{Error as ServiceError, TaskManager};
-use web3::transports::Http;
+use web3::{transports::Http, types::H160};
 
 pub fn relay(config: Configuration) -> Result<TaskManager, ServiceError> {
 	new_relay_base(config).map(|RelayBase { task_manager, .. }| task_manager)
@@ -19,6 +24,7 @@ pub fn new_relay_base(config: Configuration) -> Result<RelayBase, ServiceError> 
 		EthClientConfiguration {
 			call_interval: BFC_CALL_INTERVAL_MS,
 			block_queue_size: BFC_BLOCK_QUEUE_SIZE,
+			socket_address: H160::from_str(BFC_SOCKET_CONTRACT_ADDRESS).unwrap(),
 		},
 	);
 	let mut bfc_event_detector = EventDetector::new(bfc_client);
