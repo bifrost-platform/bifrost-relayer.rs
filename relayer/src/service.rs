3f537a0739a65da50adb09ff55007876b3751328
dnjscksdn98 <dnjscksdn98@gmail.com>
2023-03-17 18:06:15 +0900

    CCCP-6, chore: deactivate transaction manager

--- relayer/src/service.rs
+++ relayer/src/service.rs
@@ -1,11 +1,21 @@
 use cc_cli::Configuration;
-use cccp_primitives::EVMTransactionManager;
+use cccp_client::eth::{EthClient, EventDetector};
+use cccp_primitives::{
+	eth::{
+		bfc_testnet::{BFC_CALL_INTERVAL_MS, BFC_SOCKET_CONTRACT_ADDRESS},
+		bsc_testnet::{BSC_CALL_INTERVAL_MS, BSC_SOCKET_CONTRACT_ADDRESS},
+		eth_testnet::{ETH_CALL_INTERVAL_MS, ETH_SOCKET_CONTRACT_ADDRESS},
+		polygon_testnet::{POLYGON_CALL_INTERVAL_MS, POLYGON_SOCKET_CONTRACT_ADDRESS},
+		EthClientConfiguration,
+	},
+	EVMTransactionManager,
+};
 use sc_service::{Error as ServiceError, TaskManager};
-use std::sync::Arc;
+use std::{str::FromStr, sync::Arc};
 
 use ethers::{
 	providers::{Http, Provider},
-	types::Address,
+	types::{Address, H160},
 };
 
 pub fn relay(config: Configuration) -> Result<TaskManager, ServiceError> {
@@ -16,15 +26,81 @@ pub fn new_relay_base(config: Configuration) -> Result<RelayBase, ServiceError> 
 	let task_manager = TaskManager::new(config.tokio_handle, None)?;
 
 	// TODO: Workers tasks should spawn from here.
-	let client = Arc::new(Provider::<Http>::try_from(&config.private_config.eth_provider).unwrap());
-	let socket_address = "0x4A31FfeAc276CC5e508cAC0568d932d398C4DD84".parse::<Address>().unwrap();
+	// let client =
+	// Arc::new(Provider::<Http>::try_from(&config.private_config.eth_provider).unwrap());
+	// let socket_address =
+	// "0x4A31FfeAc276CC5e508cAC0568d932d398C4DD84".parse::<Address>().unwrap();
 
-	let mut evm_manager = EVMTransactionManager::new(client, socket_address).unwrap();
+	// let mut evm_manager = EVMTransactionManager::new(client, socket_address).unwrap();
+
+	// task_manager.spawn_essential_handle().spawn_blocking(
+	// 	"bfc-tx-manager",
+	// 	Some("txmanager"),
+	// 	async move { evm_manager.run().await },
+	// );
+
+	// TODO: add event detectors for every evm-chain
+	// TODO: add --chain cli option (dev, testnet, mainnet)
+
+	let bfc_client = EthClient::new(
+		Arc::new(Provider::<Http>::try_from(&config.private_config.bfc_provider).unwrap()),
+		EthClientConfiguration {
+			name: "bfc-testnet".to_string(),
+			call_interval: BFC_CALL_INTERVAL_MS,
+			socket_address: H160::from_str(BFC_SOCKET_CONTRACT_ADDRESS).unwrap(),
+		},
+	);
+	let mut bfc_event_detector = EventDetector::new(bfc_client);
+
+	let eth_client = EthClient::new(
+		Arc::new(Provider::<Http>::try_from(&config.private_config.eth_provider).unwrap()),
+		EthClientConfiguration {
+			name: "eth-testnet".to_string(),
+			call_interval: ETH_CALL_INTERVAL_MS,
+			socket_address: H160::from_str(ETH_SOCKET_CONTRACT_ADDRESS).unwrap(),
+		},
+	);
+	let mut eth_event_detector = EventDetector::new(eth_client);
+
+	let bsc_client = EthClient::new(
+		Arc::new(Provider::<Http>::try_from(&config.private_config.bsc_provider).unwrap()),
+		EthClientConfiguration {
+			name: "bsc-testnet".to_string(),
+			call_interval: BSC_CALL_INTERVAL_MS,
+			socket_address: H160::from_str(BSC_SOCKET_CONTRACT_ADDRESS).unwrap(),
+		},
+	);
+	let mut bsc_event_detector = EventDetector::new(bsc_client);
+
+	let polygon_client = EthClient::new(
+		Arc::new(Provider::<Http>::try_from(&config.private_config.polygon_provider).unwrap()),
+		EthClientConfiguration {
+			name: "polygon-testnet".to_string(),
+			call_interval: POLYGON_CALL_INTERVAL_MS,
+			socket_address: H160::from_str(POLYGON_SOCKET_CONTRACT_ADDRESS).unwrap(),
+		},
+	);
+	let mut polygon_event_detector = EventDetector::new(polygon_client);
 
 	task_manager.spawn_essential_handle().spawn_blocking(
-		"bfc-tx-manager",
-		Some("txmanager"),
-		async move { evm_manager.run().await },
+		"bfc-event-detector",
+		Some("events"),
+		async move { bfc_event_detector.run().await },
+	);
+	task_manager.spawn_essential_handle().spawn_blocking(
+		"eth-event-detector",
+		Some("events"),
+		async move { eth_event_detector.run().await },
+	);
+	task_manager.spawn_essential_handle().spawn_blocking(
+		"bsc-event-detector",
+		Some("events"),
+		async move { bsc_event_detector.run().await },
+	);
+	task_manager.spawn_essential_handle().spawn_blocking(
+		"polygon-event-detector",
+		Some("events"),
+		async move { polygon_event_detector.run().await },
 	);
 
 	Ok(RelayBase { task_manager })
