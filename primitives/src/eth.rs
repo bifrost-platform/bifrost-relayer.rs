39f1edbc472c901e18421742159e6f5f770fe858
alstjd0921 <kwonarseus@gmail.com>
2023-03-20 23:40:50 +0900

    CCCP-45, Refactor: Remove unused constants

--- primitives/src/eth.rs
+++ primitives/src/eth.rs
@@ -2,22 +2,6 @@ use ethers::{providers::ProviderError, types::H160};
 
 pub type EthResult<T = ()> = Result<T, ProviderError>;
 
-pub mod bfc_testnet {
-	pub const BFC_CHAIN_ID: u32 = 49088;
-}
-
-pub mod eth_testnet {
-	pub const ETH_CHAIN_ID: u32 = 5;
-}
-
-pub mod bsc_testnet {
-	pub const BSC_CHAIN_ID: u32 = 97;
-}
-
-pub mod polygon_testnet {
-	pub const POLYGON_CHAIN_ID: u32 = 80001;
-}
-
 /// The socket event signature.
 pub const SOCKET_EVENT_SIG: &str =
 	"0x918454f530580823dd0d8cf59cacb45a6eb7cc62f222d7129efba5821e77f191";
