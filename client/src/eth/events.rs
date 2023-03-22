52076e277c6f9835c0d114d2be3bd4fcfc32ab85
alstjd0921 <kwonarseus@gmail.com>
2023-03-22 14:35:43 +0900

    CCCP-51, Refactor: Remove unused declares

--- client/src/eth/events.rs
+++ client/src/eth/events.rs
@@ -1,19 +1,7 @@
-use super::{BlockChannel, EthClient};
+use super::EthClient;
 
-use std::{str::FromStr, sync::Arc};
-
-use cccp_primitives::eth::SOCKET_EVENT_SIG;
-use ethers::{
-	abi::RawLog,
-	prelude::decode_logs,
-	providers::JsonRpcClient,
-	types::{TransactionReceipt, H256, U64},
-};
-use tokio::{
-	sync::mpsc::Sender,
-	time::{sleep, Duration},
-};
-use tokio_stream::StreamExt;
+use ethers::types::U64;
+use tokio::sync::mpsc::Sender;
 
 pub struct EventChannel {
 	// TODO: use transaction data as message type
@@ -25,15 +13,4 @@ impl EventChannel {
 	pub fn new(channel: Sender<u32>, id: u32) -> Self {
 		Self { channel, id }
 	}
-}
-
-#[async_trait::async_trait]
-pub trait EventHandler {
-	fn new(event_channels: Arc<Vec<EventChannel>>, block_channel: Arc<BlockChannel>) -> Self;
-
-	async fn run(&self);
-
-	async fn process_confirmed_transaction(&self, receipt: TransactionReceipt);
-
-	// TODO: add common methods
 }
