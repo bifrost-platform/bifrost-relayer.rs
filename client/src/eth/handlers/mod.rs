50c5016526072bc51094be75bcd52ee2a30417cb
alstjd0921 <kwonarseus@gmail.com>
2023-03-22 14:08:29 +0900

    CCCP-51, Feature: Declare Handler trait

--- client/src/eth/handlers/mod.rs
+++ client/src/eth/handlers/mod.rs
@@ -1,2 +1,17 @@
 mod socket_handler;
+
+use ethers::types::{Transaction, TransactionReceipt, H256};
 pub use socket_handler::*;
+
+#[async_trait::async_trait]
+pub trait Handler {
+	async fn run(&self);
+
+	async fn process_confirmed_transaction(&self, receipt: TransactionReceipt);
+
+	async fn send_transaction(&self, transaction: Transaction);
+
+	fn is_target_contract(&self, receipt: TransactionReceipt) -> bool;
+
+	fn is_target_event(topic: H256) -> bool;
+}
