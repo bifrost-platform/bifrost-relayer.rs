001c7b92e4da9558a58bee1e124273c101ca7e16
noah <jinwook@pi-lab.io>
2023-03-27 14:53:19 +0900

    CCCP-54, test: locally managing nonce with simple send_transaction

--- client/src/eth/tx.rs
+++ client/src/eth/tx.rs
@@ -62,3 +62,62 @@ impl<T: JsonRpcClient> TransactionManager<T> {
 		Ok(())
 	}
 }
+
+#[cfg(test)]
+mod tests {
+	use std::time::Duration;
+
+	use ethers::{
+		prelude::MiddlewareBuilder,
+		providers::{Http, Middleware, Provider},
+		types::{BlockNumber, TransactionRequest},
+		utils::{Anvil, AnvilInstance},
+	};
+
+	pub fn spawn_anvil() -> (Provider<Http>, AnvilInstance) {
+		let anvil = Anvil::new().block_time(1u64).spawn();
+		let provider = Provider::<Http>::try_from(anvil.endpoint())
+			.unwrap()
+			.interval(Duration::from_millis(50u64));
+		(provider, anvil)
+	}
+
+	#[tokio::test]
+	async fn nonce_manager() {
+		let (provider, anvil) = spawn_anvil();
+		let address = anvil.addresses()[0];
+		let to = anvil.addresses()[1];
+
+		let provider = provider.nonce_manager(address);
+
+		let nonce = provider
+			.get_transaction_count(address, Some(BlockNumber::Pending.into()))
+			.await
+			.unwrap()
+			.as_u64();
+
+		println!("nonce : {:?}", nonce);
+
+		let num_tx = 3;
+		let mut tx_hashes = Vec::with_capacity(num_tx);
+		for _ in 0..num_tx {
+			let tx = provider
+				.send_transaction(
+					TransactionRequest::new().from(address).to(to).value(100u64),
+					None,
+				)
+				.await
+				.unwrap();
+			tx_hashes.push(*tx);
+		}
+
+		let mut nonces = Vec::with_capacity(num_tx);
+		for tx_hash in tx_hashes {
+			nonces.push(provider.get_transaction(tx_hash).await.unwrap().unwrap().nonce.as_u64());
+		}
+
+		println!("nonces : {:?}", nonces);
+
+		assert_eq!(nonces, (nonce..nonce + num_tx as u64).collect::<Vec<_>>());
+	}
+}
