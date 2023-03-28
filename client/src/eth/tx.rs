a76cf23242fdd88bbce6cc2d017ee657c1421378
noah <jinwook@pi-lab.io>
2023-03-28 11:02:30 +0900

    CCCP-54, refactor: try to resend the tx if there is no corresponding receipt

--- client/src/eth/tx.rs
+++ client/src/eth/tx.rs
@@ -19,7 +19,7 @@ pub struct TransactionManager<T> {
 	pub receiver: UnboundedReceiver<TransactionRequest>,
 }
 
-impl<T: JsonRpcClient> TransactionManager<T> {
+impl<T: 'static + JsonRpcClient> TransactionManager<T> {
 	/// Instantiates a new `TransactionManager` instance.
 	pub fn new(client: Arc<EthClient<T>>) -> (Self, UnboundedSender<TransactionRequest>) {
 		let (sender, receiver) = mpsc::unbounded_channel::<TransactionRequest>();
@@ -45,19 +45,23 @@ impl<T: JsonRpcClient> TransactionManager<T> {
 		let nonce = self.middleware.get_transaction_count(msg.from.unwrap(), None).await.unwrap();
 
 		let (max_fee_per_gas, _max_priority_fee_per_gas) =
-			self.middleware.estimate_eip1559_fees(None).await.unwrap();
+			self.middleware.estimate_eip1559_fees(None).await?;
 
 		let tx = msg.gas(U256::from(1000000)).gas_price(max_fee_per_gas).nonce(nonce);
 
-		let res = self
-			.middleware
-			.send_transaction(tx, None)
-			.await
-			.expect("fail to sending tx")
-			.await
-			.unwrap();
+		let pending_tx = self.middleware.send_transaction(tx.clone(), None).await?;
 
-		println!("transaction result : {:?}", res);
+		let receipt = pending_tx.await?.ok_or("tx dropped".to_string())?;
+
+		println!("transaction result : {:?}", receipt);
+
+		match self.middleware.get_transaction(receipt.transaction_hash).await? {
+			Some(t) => println!("Sent tx: {:?}", t),
+			None => {
+				println!("retry to send tx");
+				self.middleware.send_transaction(tx, None).await?.await?;
+			},
+		}
 
 		Ok(())
 	}
@@ -70,12 +74,13 @@ mod tests {
 	use ethers::{
 		prelude::MiddlewareBuilder,
 		providers::{Http, Middleware, Provider},
+		signers::LocalWallet,
 		types::{BlockNumber, TransactionRequest},
 		utils::{Anvil, AnvilInstance},
 	};
 
 	pub fn spawn_anvil() -> (Provider<Http>, AnvilInstance) {
-		let anvil = Anvil::new().block_time(1u64).spawn();
+		let anvil = Anvil::new().block_time(1u64).chain_id(1337_u64).spawn();
 		let provider = Provider::<Http>::try_from(anvil.endpoint())
 			.unwrap()
 			.interval(Duration::from_millis(50u64));
@@ -111,12 +116,70 @@ mod tests {
 			tx_hashes.push(*tx);
 		}
 
+		println!("tx_hashes : {:?}", &tx_hashes);
+
 		let mut nonces = Vec::with_capacity(num_tx);
 		for tx_hash in tx_hashes {
 			nonces.push(provider.get_transaction(tx_hash).await.unwrap().unwrap().nonce.as_u64());
 		}
 
 		println!("nonces : {:?}", nonces);
+
+		assert_eq!(nonces, (nonce..nonce + num_tx as u64).collect::<Vec<_>>());
+	}
+
+	#[tokio::test]
+	async fn send_transaction() {
+		let (provider, anvil) = spawn_anvil();
+		let wallet1: LocalWallet = anvil.keys()[0].clone().into();
+		let address = wallet1.address;
+
+		let wallet2: LocalWallet = anvil.keys()[1].clone().into();
+		let to = wallet2.address;
+
+		let provider = provider;
+		// let provider = provider.with_signer(wallet1).nonce_manager(address);
+
+		let nonce = provider
+			.get_transaction_count(address, Some(BlockNumber::Pending.into()))
+			.await
+			.unwrap()
+			.as_u64();
+
+		let num_tx = 3;
+		let mut tx_hashes = Vec::with_capacity(num_tx);
+		for _ in 0..num_tx {
+			let nonce = provider.get_transaction_count(address, None).await.unwrap();
+			println!("nonce is increased? : {:?}", nonce);
+
+			let tx = provider
+				.send_transaction(
+					TransactionRequest::new().from(address).to(to).value(100u64),
+					None,
+				)
+				.await
+				.unwrap()
+				.await
+				.unwrap();
+
+			tx_hashes.push(tx.unwrap());
+		}
+		println!("tx : {:?}", &tx_hashes);
+
+		let mut nonces = Vec::with_capacity(num_tx);
+		for tx_hash in tx_hashes {
+			nonces.push(
+				provider
+					.get_transaction(tx_hash.transaction_hash)
+					.await
+					.unwrap()
+					.unwrap()
+					.nonce
+					.as_u64(),
+			);
+		}
+
+		println!("nonces : {:?}", &nonces);
 
 		assert_eq!(nonces, (nonce..nonce + num_tx as u64).collect::<Vec<_>>());
 	}
