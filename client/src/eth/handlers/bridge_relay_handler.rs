dce2f5abebe739bbb6caf1c4e5664bcc07b6d8e2
Alex Won <dnjscksdn98@gmail.com>
2023-04-17 17:53:01 +0900

    CCCP-60, feature: handle socket event rollback cases (#26)
    * CCCP-60, feature: process reverted relay transactions
    
    * CCCP-60, fix: fetch socket event sig by macro
    
    * CCCP-60, chore: verify sentry dsn before init
    
    * CCCP-60, chore: replace to WalletResult
    
    * CCCP-60, chore: update logs
    
    * CCCP-60, chore: apply cargo-clippy
    
    * CCCP-60, fix: handle Failed socket event status
    
    * CCCP-81, chore: use dynamic gas estimation and handle on error
    
    * CCCP-81, chore: remove resolved todo comments
    
    * CCCP-81, chore: apply cargo-clippy

--- client/src/eth/handlers/bridge_relay_handler.rs
+++ client/src/eth/handlers/bridge_relay_handler.rs
@@ -1,11 +1,11 @@
-use std::{str::FromStr, sync::Arc};
+use std::sync::Arc;
 
-// TODO: Move event sig into handler structure (Initialize from config.yaml)
 use cccp_primitives::{
 	contracts::socket_external::{
 		BridgeRelayBuilder, PollSubmit, Signatures, SocketEvents, SocketExternal, SocketMessage,
 	},
-	eth::{BridgeDirection, Contract, SocketEventStatus, SOCKET_EVENT_SIG},
+	eth::{BridgeDirection, Contract, SocketEventStatus},
+	socket_external::SerializedPoll,
 	sub_display_format,
 };
 use ethers::{
@@ -39,6 +39,8 @@ pub struct BridgeRelayHandler<T> {
 	pub target_socket: SocketExternal<Provider<T>>,
 	/// The socket contracts supporting CCCP.
 	pub socket_contracts: Vec<Contract>,
+	/// Signature of the `Socket` Event.
+	pub socket_signature: H256,
 }
 
 impl<T: JsonRpcClient> BridgeRelayHandler<T> {
@@ -51,13 +53,16 @@ impl<T: JsonRpcClient> BridgeRelayHandler<T> {
 		target_socket: H160,
 		socket_contracts: Vec<Contract>,
 	) -> Self {
+		let target_socket = SocketExternal::new(target_socket, client.provider.clone());
+		let socket_signature = target_socket.abi().event("Socket").unwrap().signature();
 		Self {
 			event_senders,
 			block_receiver,
-			client: client.clone(),
+			client,
 			target_contract,
-			target_socket: SocketExternal::new(target_socket, client.provider.clone()),
+			target_socket,
 			socket_contracts,
+			socket_signature,
 		}
 	}
 }
@@ -86,6 +91,12 @@ impl<T: JsonRpcClient> Handler for BridgeRelayHandler<T> {
 
 	async fn process_confirmed_transaction(&self, receipt: TransactionReceipt) {
 		if self.is_target_contract(&receipt) {
+			let status = receipt.status.unwrap();
+			if status.is_zero() {
+				self.process_reverted_transaction(receipt).await;
+				return
+			}
+
 			let mut stream = tokio_stream::iter(receipt.logs);
 
 			while let Some(log) = stream.next().await {
@@ -94,6 +105,7 @@ impl<T: JsonRpcClient> Handler for BridgeRelayHandler<T> {
 					match decode_logs::<SocketEvents>(&[raw_log]) {
 						Ok(decoded) => match &decoded[0] {
 							SocketEvents::Socket(socket) => {
+								let status = SocketEventStatus::from_u8(socket.msg.status);
 								let src_chain_id = u32::from_be_bytes(socket.msg.req_id.chain);
 								let dst_chain_id = u32::from_be_bytes(socket.msg.ins_code.chain);
 								let is_inbound = self.is_inbound_sequence(dst_chain_id);
@@ -104,7 +116,7 @@ impl<T: JsonRpcClient> Handler for BridgeRelayHandler<T> {
 									} else {
 										"Outbound".to_string()
 									},
-									SocketEventStatus::from_u8(socket.msg.status),
+									status,
 									socket.msg.req_id.sequence,
 									src_chain_id,
 									dst_chain_id,
@@ -127,7 +139,7 @@ impl<T: JsonRpcClient> Handler for BridgeRelayHandler<T> {
 							"[{}]-[{}] Unknown error while decoding socket event: {}",
 							self.client.get_chain_name(),
 							SUB_LOG_TARGET,
-							error.to_string()
+							error
 						),
 					}
 				}
@@ -147,7 +159,7 @@ impl<T: JsonRpcClient> Handler for BridgeRelayHandler<T> {
 	}
 
 	fn is_target_event(&self, topic: H256) -> bool {
-		topic == H256::from_str(SOCKET_EVENT_SIG).unwrap()
+		topic == self.socket_signature
 	}
 }
 
@@ -183,7 +195,7 @@ impl<T: JsonRpcClient> BridgeRelayBuilder for BridgeRelayHandler<T> {
 		if is_inbound {
 			// build signatures for inbound requests
 			match status {
-				SocketEventStatus::Requested => Signatures::default(),
+				SocketEventStatus::Requested | SocketEventStatus::Failed => Signatures::default(),
 				SocketEventStatus::Executed => {
 					msg.status = SocketEventStatus::Accepted.into();
 					Signatures::from(self.sign_socket_message(msg).await)
@@ -264,6 +276,81 @@ impl<T: JsonRpcClient> BridgeRelayBuilder for BridgeRelayHandler<T> {
 }
 
 impl<T: JsonRpcClient> BridgeRelayHandler<T> {
+	/// (Re-)Handle the reverted relay transaction. This method only handles if it was an
+	/// Inbound-Requested or Outbound-Accepted sequence. This will let the sequence follow the
+	/// fail-case flow.
+	async fn process_reverted_transaction(&self, receipt: TransactionReceipt) {
+		// only handles owned transactions
+		if self.is_owned_relay_transaction(&receipt) {
+			if let Some(tx) = self.client.get_transaction(receipt.transaction_hash).await.unwrap() {
+				// the reverted transaction must be execution of `poll()`
+				let selector = &tx.input[0..4];
+				let poll_selector =
+					self.target_socket.abi().function("poll").unwrap().short_signature();
+				if selector == poll_selector {
+					match self
+						.target_socket
+						.decode_with_selector::<SerializedPoll, Bytes>(poll_selector, tx.input)
+					{
+						Ok(mut poll) => {
+							let prev_status = SocketEventStatus::from_u8(poll.msg.status);
+							let src_chain_id = u32::from_be_bytes(poll.msg.req_id.chain);
+							let dst_chain_id = u32::from_be_bytes(poll.msg.ins_code.chain);
+							let is_inbound = self.is_inbound_sequence(dst_chain_id);
+
+							if is_inbound && matches!(prev_status, SocketEventStatus::Requested) {
+								// if inbound-Requested
+								poll.msg.status = SocketEventStatus::Failed.into();
+							} else if !is_inbound &&
+								matches!(prev_status, SocketEventStatus::Accepted)
+							{
+								// if outbound-Accepted
+								poll.msg.status = SocketEventStatus::Rejected.into();
+							} else {
+								return
+							}
+
+							let metadata = BridgeRelayMetadata::new(
+								if is_inbound {
+									"Inbound".to_string()
+								} else {
+									"Outbound".to_string()
+								},
+								SocketEventStatus::from_u8(poll.msg.status),
+								poll.msg.req_id.sequence,
+								src_chain_id,
+								dst_chain_id,
+							);
+
+							log::info!(
+								target: &self.client.get_chain_name(),
+								"-[{}] ♻️  Re-Processed reverted relay transaction: {}, Reverted at: {:?}-{:?}",
+								sub_display_format(SUB_LOG_TARGET),
+								metadata,
+								receipt.block_number.unwrap(),
+								receipt.transaction_hash,
+							);
+
+							self.send_socket_message(poll.msg, metadata, is_inbound).await;
+						},
+						Err(error) => {
+							// ignore for now if function input data decode fails
+							log::warn!(
+								target: &self.client.get_chain_name(),
+								"-[{}] ⚠️  Tried to re-process the reverted relay transaction but failed to decode function input: {}, Reverted at: {:?}-{:?}",
+								sub_display_format(SUB_LOG_TARGET),
+								error.to_string(),
+								receipt.block_number.unwrap(),
+								receipt.transaction_hash,
+							);
+							sentry::capture_error(&error);
+						},
+					}
+				}
+			}
+		}
+	}
+
 	/// Sends the `SocketMessage` to the target chain channel.
 	async fn send_socket_message(
 		&self,
@@ -301,6 +388,7 @@ impl<T: JsonRpcClient> BridgeRelayHandler<T> {
 	) -> u32 {
 		match status {
 			SocketEventStatus::Requested |
+			SocketEventStatus::Failed |
 			SocketEventStatus::Executed |
 			SocketEventStatus::Reverted => dst_chain_id,
 			SocketEventStatus::Accepted | SocketEventStatus::Rejected => src_chain_id,
@@ -348,6 +436,12 @@ impl<T: JsonRpcClient> BridgeRelayHandler<T> {
 		)
 	}
 
+	/// Verifies whether the current relayer owns the relay transaction.
+	fn is_owned_relay_transaction(&self, receipt: &TransactionReceipt) -> bool {
+		ethers::utils::to_checksum(&receipt.from, None) ==
+			ethers::utils::to_checksum(&self.client.address(), None)
+	}
+
 	/// Request send bridge relay transaction to the target event channel.
 	fn request_send_transaction(
 		&self,
@@ -381,6 +475,74 @@ impl<T: JsonRpcClient> BridgeRelayHandler<T> {
 						error.to_string()
 					);
 					sentry::capture_error(&error);
+				},
+			}
+		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use cccp_primitives::socket_external::SerializedPoll;
+	use ethers::{
+		providers::{Http, Middleware, Provider},
+		types::H160,
+	};
+	use std::{str::FromStr, sync::Arc};
+
+	#[tokio::test]
+	async fn function_decode() {
+		let provider = Arc::new(Provider::<Http>::try_from("").unwrap());
+
+		let target_socket = SocketExternal::new(
+			H160::from_str("0x0218371b18340aBD460961bdF3Bd5F01858dAB53").unwrap(),
+			provider.clone(),
+		);
+		let poll_selector = target_socket.abi().function("poll").unwrap().short_signature();
+
+		let tx = provider
+			.get_transaction(
+				H256::from_str(
+					"0xbef5fc7d225eb4835f030fc5ccb8b9fe12c33943b89a5588aa6b618d796c8b80",
+				)
+				.unwrap(),
+			)
+			.await
+			.unwrap();
+
+		if let Some(tx) = tx {
+			match target_socket
+				.decode_with_selector::<SerializedPoll, Bytes>(poll_selector, tx.input)
+			{
+				Ok(mut poll) => {
+					let status = SocketEventStatus::from_u8(poll.msg.status);
+					let src_chain_id = u32::from_be_bytes(poll.msg.req_id.chain);
+					let dst_chain_id = u32::from_be_bytes(poll.msg.ins_code.chain);
+					let is_inbound = true;
+
+					if is_inbound && matches!(status, SocketEventStatus::Requested) {
+						// if inbound-Requested
+						poll.msg.status = SocketEventStatus::Failed.into();
+					} else if !is_inbound && matches!(status, SocketEventStatus::Accepted) {
+						// if outbound-Accepted
+						poll.msg.status = SocketEventStatus::Rejected.into();
+					} else {
+						println!("failed");
+						return
+					}
+
+					let metadata = BridgeRelayMetadata::new(
+						if is_inbound { "Inbound".to_string() } else { "Outbound".to_string() },
+						SocketEventStatus::from_u8(poll.msg.status),
+						poll.msg.req_id.sequence,
+						src_chain_id,
+						dst_chain_id,
+					);
+					println!("{}", metadata);
+				},
+				Err(error) => {
+					println!("error -> {:?}", error);
 				},
 			}
 		}
