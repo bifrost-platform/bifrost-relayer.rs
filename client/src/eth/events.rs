774671522a963d401e8e03a152041f4ead23e46d
Alex Won <dnjscksdn98@gmail.com>
2023-05-03 12:29:30 +0900

    CCCP-113, chore: update logs and sentry messages (#35)
    * CCCP-113, chore: update tx-manager logs and sentry messages
    
    * CCCP-113, feature: multiply the retry interval
    
    * CCCP-113, refactor: simplify event message retry
    
    * CCCP-113, chore: rename method and doc string

--- client/src/eth/events.rs
+++ client/src/eth/events.rs
@@ -17,6 +17,12 @@ use br_primitives::{
 
 use super::{traits::BootstrapHandler, EthClient};
 
+/// The default retry interval in milliseconds.
+pub const DEFAULT_RETRY_INTERVAL_MS: u64 = 3000;
+
+/// The coefficient that will be multiplied to the retry interval on every new retry.
+pub const RETRY_COEFFICIENT: u64 = 2;
+
 #[derive(Clone, Debug)]
 /// The message format passed through the block channel.
 pub struct EventMessage {
