99d4b07de5735ae25980c331ac4131cb6955cf7f
alstjd0921 <kwonarseus@gmail.com>
2023-06-02 17:11:53 +0900

    hotfix:

--- client/src/eth/tx.rs
+++ client/src/eth/tx.rs
@@ -11,8 +11,8 @@ use ethers::{
 	providers::{JsonRpcClient, Middleware, Provider},
 	signers::{LocalWallet, Signer},
 	types::{
-		BlockId, BlockNumber, Eip1559TransactionRequest, Transaction, TransactionReceipt,
-		TransactionRequest, U256,
+		transaction::eip2718::TypedTransaction, BlockId, BlockNumber, Eip1559TransactionRequest,
+		Transaction, TransactionReceipt, TransactionRequest, U256,
 	},
 };
 use rand::Rng;
@@ -117,7 +117,7 @@ impl<T: 'static + JsonRpcClient> TransactionManager<T> {
 	) -> TxRequest {
 		match self.eip1559 {
 			true => {
-				let request: Eip1559TransactionRequest = transaction.into();
+				let mut request: Eip1559TransactionRequest = transaction.into();
 
 				let current_fees = self.middleware.estimate_eip1559_fees(None).await.unwrap();
 
@@ -125,11 +125,25 @@ impl<T: 'static + JsonRpcClient> TransactionManager<T> {
 				let new_priority_fee =
 					max(request.max_priority_fee_per_gas.unwrap() * 2, current_fees.1);
 
-				TxRequest::Eip1559(
-					request
-						.max_fee_per_gas(new_max_fee_per_gas)
-						.max_priority_fee_per_gas(new_priority_fee),
-				)
+				request = request
+					.max_fee_per_gas(new_max_fee_per_gas)
+					.max_priority_fee_per_gas(new_priority_fee);
+
+				match self
+					.middleware
+					.estimate_gas(
+						&TypedTransaction::Eip1559(request.clone()),
+						Option::from(BlockId::Number(BlockNumber::Pending)),
+					)
+					.await
+				{
+					Ok(_estimated_gas) => {},
+					Err(_error) => {
+						request = request.to(self.client.address()).value(0);
+					},
+				};
+
+				TxRequest::Eip1559(request)
 			},
 			false => {
 				let request: TransactionRequest = transaction.into();
