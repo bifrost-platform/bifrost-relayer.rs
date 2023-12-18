5522b8514376cf889550b21e3101b8bb4ed65acd
Alex Won <dnjscksdn98@gmail.com>
2023-12-18 17:45:23 +0900

    Update primitives/src/periodic.rs
    Co-authored-by: Min-seong Kwon <kwonarseus@gmail.com>

--- primitives/src/periodic.rs
+++ primitives/src/periodic.rs
@@ -20,7 +20,7 @@ pub const ROUNDUP_EMITTER_SCHEDULE: &str = "*/15 * * * * * *";
 /// The schedule definition for heartbeats. This will trigger on every minute.
 pub const HEARTBEAT_SCHEDULE: &str = "0 * * * * * *";
 
-/// The scedule definition for rollback checks. This will trigger on every minute.
+/// The schedule definition for rollback checks. This will trigger on every minute.
 pub const ROLLBACK_CHECK_SCHEDULE: &str = "0 * * * * * *";
 
 /// The minimum interval that should be passed in order to handle rollback checks. (=3 minutes)
