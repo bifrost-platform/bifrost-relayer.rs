5bbcb9f54d093dd93d7e2ece3a9c96b3b87ec9e7
Alex Won <dnjscksdn98@gmail.com>
2024-04-29 16:13:56 +0900

    Alex/minor fixes (2) (#127)
    * feat: prevent bitcoin inbound processing when it is already ended
    
    * fix: remove todo comments
    
    * feat: prevent bitcoin socket poll when already voted

--- client/src/eth/mod.rs
+++ client/src/eth/mod.rs
@@ -74,6 +74,10 @@ pub struct EthClient<T> {
 		self.metadata.id
 	}
 
+	pub fn get_bitcoin_chain_id(&self) -> Option<ChainID> {
+		self.metadata.bitcoin_chain_id
+	}
+
 	/// Returns the provider URL.
 	pub fn get_url(&self) -> Url {
 		self.metadata.url.clone()
