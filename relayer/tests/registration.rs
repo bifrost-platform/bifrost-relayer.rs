8198b7da03950f42de1cfc7b3710a889edd6604b
SunoukMun <mun_smith@pi-lab.io>
2024-06-21 10:26:17 +0900

    fix: async registration

--- relayer/tests/registration.rs
+++ relayer/tests/registration.rs
@@ -65,9 +65,9 @@ async fn test_user_registration() {
 }
 
 #[tokio::test]
-async fn test_numerous_register_request() {
+async fn test_numerous_register_keypair() {
 	let amount_in_wei = 1_000_000_000_000_000_000u128; // 1 ETH in wei
-	let number_of_user_request = 10;
+	let number_of_user_request = 110;
 
 	let wallet_name = "sunouk";
 	// let wallet_name1 = "sunouk3";
@@ -75,7 +75,7 @@ async fn test_numerous_register_request() {
 
 	let keypair_path = KEYPAIR_PATH.to_string() + "/keypair.json";
 
-	let mut tasks = vec![];
+	// let mut tasks = vec![];
 
 	let mut wallet_details_list = read_wallet_details(&keypair_path).await;
 
@@ -87,8 +87,6 @@ async fn test_numerous_register_request() {
 		let (wallet, _) = create_new_account().await.unwrap();
 		let priv_key = wallet.signer().to_bytes();
 		let priv_key_hex = format!("0x{}", hex::encode(priv_key));
-
-		transfer_fund(&priv_key_hex, amount_in_wei, ALITH_PRIV_KEY).await.unwrap();
 
 		let (bfc_client, btc_provider) = test_set_bfc_client(&priv_key_hex).await;
 
@@ -114,7 +112,70 @@ async fn test_numerous_register_request() {
 			vault_address: "".to_string(),
 		});
 
+		// 	tasks.push(tokio::spawn(async move {
+		// 		registration(&refund_address, bfc_client).await;
+		// 	}));
+		// }
+
+		// let results = futures::future::join_all(tasks).await;
+
+		// for result in results {
+		// 	match result {
+		// 		Ok(()) => {
+		// 			println!("Registration succeeded");
+		// 		},
+		// 		Err(e) => {
+		// 			println!("Registration error: {:?}", e);
+		// 			assert!(false, "Registration failed");
+		// 		},
+		// 	}
+		// }
+
+		// tasks.push(tokio::spawn(async move {
+		// 	transfer_fund(&priv_key_hex.clone(), amount_in_wei, ALITH_PRIV_KEY)
+		// 		.await
+		// 		.unwrap();
+		// }));
+
+		transfer_fund(&priv_key_hex.clone(), amount_in_wei, ALITH_PRIV_KEY)
+			.await
+			.unwrap();
+	}
+
+	// let results = futures::future::join_all(tasks).await;
+
+	// for result in results {
+	// 	match result {
+	// 		Ok(()) => {
+	// 			println!("Registration succeeded");
+	// 		},
+	// 		Err(e) => {
+	// 			println!("Registration error: {:?}", e);
+	// 			assert!(false, "Registration failed");
+	// 		},
+	// 	}
+	// }
+
+	// Write wallet details to a JSON file
+	let json_data = serde_json::to_string_pretty(&wallet_details_list).unwrap();
+	let mut file = File::create(keypair_path).unwrap();
+	file.write_all(json_data.as_bytes()).unwrap();
+}
+
+#[tokio::test]
+async fn test_numerous_register_request() {
+	let keypair_path = KEYPAIR_PATH.to_string() + "/keypair.json";
+
+	let wallet_details_list = read_wallet_details(&keypair_path).await;
+
+	let mut tasks = vec![];
+
+	for wallet_details in &wallet_details_list {
+		let priv_key = wallet_details.priv_key.clone();
+		let refund_address = wallet_details.refund_address.clone();
+
 		tasks.push(tokio::spawn(async move {
+			let (bfc_client, _) = test_set_bfc_client(&priv_key).await;
 			registration(&refund_address, bfc_client).await;
 		}));
 	}
@@ -132,11 +193,6 @@ async fn test_numerous_register_request() {
 			},
 		}
 	}
-
-	// Write wallet details to a JSON file
-	let json_data = serde_json::to_string_pretty(&wallet_details_list).unwrap();
-	let mut file = File::create(keypair_path).unwrap();
-	file.write_all(json_data.as_bytes()).unwrap();
 }
 
 #[tokio::test]
