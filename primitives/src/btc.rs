c6f80d76388a9d27846de15c8f378927d150e1d7
alstjd0921 <kwonarseus@gmail.com>
2025-04-14 17:25:14 +0900

    CCCP-511, feature: implement btc feerate feeder

--- primitives/src/btc.rs
+++ primitives/src/btc.rs
@@ -1,3 +1,15 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FeeRateResponse {
+	#[serde(rename = "fastestFee")]
+	pub fastest_fee: u64,
+	#[serde(rename = "economyFee")]
+	pub economy_fee: u64,
+	#[serde(rename = "minimumFee")]
+	pub minimum_fee: u64,
+}
+
 /// Mempool.space endpoint. (=block height)
 pub const MEMPOOL_SPACE_BLOCK_HEIGHT_ENDPOINT: &str = "https://mempool.space/api/blocks/tip/height";
 
