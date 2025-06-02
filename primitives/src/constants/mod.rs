d380e0caf931d4de7f5c02d959efb5a9b393925c
dnjscksdn98 <dnjscksdn98@gmail.com>
2025-06-02 13:48:29 +0900

    CCCP-511, fix: remove duplicate constants

--- primitives/src/constants/mod.rs
+++ primitives/src/constants/mod.rs
@@ -3,6 +3,3 @@ pub mod config;
 pub mod errors;
 pub mod schedule;
 pub mod tx;
-
-pub const MEMPOOL_SPACE_API: &str = "https://mempool.space/api/v1/fees/recommended";
-pub const MEMPOOL_SPACE_API_TESTNET: &str = "https://mempool.space/testnet/api/v1/fees/recommended";
