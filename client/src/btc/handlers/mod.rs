d4d33918865874cfa7d1df23bfb15a590e724e5d
dnjscksdn98 <dnjscksdn98@gmail.com>
2025-06-02 14:16:43 +0900

    CCCP-511, refactor: use macro to reduce code duplication

--- client/src/btc/handlers/mod.rs
+++ client/src/btc/handlers/mod.rs
@@ -31,6 +31,12 @@ use tokio::time::sleep;
 
 use super::block::EventMessage;
 
+macro_rules! create_xt_request_message {
+	($xt_request:expr, $metadata:expr) => {
+		XtRequestMessage::new($xt_request.into(), $metadata.clone())
+	};
+}
+
 #[async_trait::async_trait]
 pub trait XtRequester<F, P>
 where
@@ -47,36 +53,7 @@ where
 		metadata: XtRequestMetadata,
 		sub_log_target: &str,
 	) {
-		let msg = match xt_request {
-			XtRequest::SubmitSignedPsbt(call) => {
-				XtRequestMessage::new(call.into(), metadata.clone())
-			},
-			XtRequest::SubmitVaultKey(call) => XtRequestMessage::new(call.into(), metadata.clone()),
-			XtRequest::SubmitUnsignedPsbt(call) => {
-				XtRequestMessage::new(call.into(), metadata.clone())
-			},
-			XtRequest::SubmitExecutedRequest(call) => {
-				XtRequestMessage::new(call.into(), metadata.clone())
-			},
-			XtRequest::SubmitSystemVaultKey(call) => {
-				XtRequestMessage::new(call.into(), metadata.clone())
-			},
-			XtRequest::SubmitRollbackPoll(call) => {
-				XtRequestMessage::new(call.into(), metadata.clone())
-			},
-			XtRequest::VaultKeyPresubmission(call) => {
-				XtRequestMessage::new(call.into(), metadata.clone())
-			},
-			XtRequest::SubmitUtxos(call) => XtRequestMessage::new(call.into(), metadata.clone()),
-			XtRequest::BroadcastPoll(call) => XtRequestMessage::new(call.into(), metadata.clone()),
-			XtRequest::SubmitOutboundRequests(call) => {
-				XtRequestMessage::new(call.into(), metadata.clone())
-			},
-			XtRequest::SubmitFeeRate(call) => XtRequestMessage::new(call.into(), metadata.clone()),
-			XtRequest::RemoveOutboundMessages(call) => {
-				XtRequestMessage::new(call.into(), metadata.clone())
-			},
-		};
+		let msg = create_xt_request_message!(xt_request, metadata);
 		match self.xt_request_sender().send(msg) {
 			Ok(_) => log::info!(
 				target: &self.bfc_client().get_chain_name(),
