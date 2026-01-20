fe1a9cb06700f82ad1d78abd55ad0308de442a54
alstjd0921 <kwonarseus@gmail.com>
2026-01-20 15:35:04 +0900

    CCCP-524, feature: Add `SocketQueuePoller` with async handler initialization and Substrate block processing

--- client/src/eth/handlers/mod.rs
+++ client/src/eth/handlers/mod.rs
@@ -1,5 +1,7 @@
 mod roundup_relay_handler;
+mod socket_queue_poller;
 mod socket_relay_handler;
 
 pub use roundup_relay_handler::*;
+pub use socket_queue_poller::*;
 pub use socket_relay_handler::*;
