6cb65dda85b62368a42de50c496b8a87d882b68c
Min-seong Kwon <kwonarseus@gmail.com>
2026-03-11 17:36:31 +0900

    Fix nonce issue (#199)
    * refactor: switch to simple nonce management in provider configuration
    
    * chore: flush on already-known error
    
    ---------
    
    Co-authored-by: dnjscksdn98 <dnjscksdn98@gmail.com>

--- relayer/src/service.rs
+++ relayer/src/service.rs
@@ -125,7 +125,7 @@ pub async fn relay(config: Configuration) -> Result<TaskManager, ServiceError> {
 		let provider = Arc::new(
 			ProviderBuilder::new()
 				.disable_recommended_fillers()
-				.with_cached_nonce_management()
+				.with_simple_nonce_management()
 				.filler(GasFiller)
 				.filler(ChainIdFiller::new(evm_provider.id.into()))
 				.network::<AnyNetwork>()
